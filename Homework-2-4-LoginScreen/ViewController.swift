6ba2297c63ff17da0e279fd4582bc009eed510b6
Aleksey Zhurkin <web.zhurkin@gmail.com>
2024-12-06 12:27:33 +0300

    Initial Commit

---
+++ Homework-2-4-LoginScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework-2-4-LoginScreen
+//
+//  Created by Алексей Журкин on 06.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
