b86c4c64ad2b5bb454f4e7bd7207780ca6f477a5
Aleksey Zhurkin <web.zhurkin@gmail.com>
2024-12-10 18:15:43 +0300

    Structured app files

---
+++ Homework-2-4-LoginScreen/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  Homework-2-4-LoginScreen
+//
+//  Created by Алексей Журкин on 10.12.2024.
+//
+
+import Foundation
---
+++ Homework-2-4-LoginScreen/ViewControllers/WelcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeScreenViewController.swift
+//  Homework-2-4-LoginScreen
+//
+//  Created by Алексей Журкин on 06.12.2024.
+//
+
+import UIKit
+
+final class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    var user: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        welcomeLabel.text = "Welcome, \(user ?? "")!"
+    }
+}
